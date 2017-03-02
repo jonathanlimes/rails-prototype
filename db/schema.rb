09178b06ec10673a84c80393029c2518bdbbd667
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-03-02 15:25:29 +0800

    User model update

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302064224) do
+ActiveRecord::Schema.define(version: 20170302071804) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
